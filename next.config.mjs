c85b65acb1480b9e04ffcc7774792f9c49e7c6cd
mrlectus <lilbrown3000@gmail.com>
2024-12-23 16:40:57 +0100

    for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    // ppr: "incremental",
+    reactCompiler: true,
+    authInterrupts: true,
+  },
+};
 
 export default nextConfig;
